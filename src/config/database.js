ed2f1c919f208fd22139749c8b07e8552d480c55
Andre Galdino <andre.galdinolima@gmail.com>
2020-07-09 20:04:01 -0300

    chore: database connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   mongo: {
+    url: process.env.DATABASE_URL,
     host: process.env.DATABASE_HOST || 'localhost',
     port: process.env.DATABASE_PORT || 27017,
-    database: process.env.DATABASE_DATABASE || 'database',
-    user: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || 'secret',
+    database: process.env.DATABASE_DB || 'database',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
   },
 };
---
+++ src/services/connectDatabase.js
@@ -0,0 +1,30 @@
+const config = require('../config/database');
+const mongoose = require('mongoose');
+
+const databaseUrl = () => {
+  const { mongo: {url, host, database, port}} = config;
+  if (url) {
+    return url;
+  }
+
+  return `mongodb://${host}:${port}/${database}`;
+};
+
+const connect = async () => {
+  const { mongo: {user, password}} = config;
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+
+  if (user && password) {
+    options.user = user;
+    options.pass = password;
+  }
+
+  const db = await mongoose.connect(databaseUrl(), options)
+  return db;
+};
+
+module.exports.connect = connect;
