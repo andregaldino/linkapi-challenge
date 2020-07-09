c613f269f1773cf5ab3f3de052849b139bd2bcc0
Andre Galdino <andre.galdinolima@gmail.com>
2020-07-09 15:21:50 -0300

    chore: callback for deal updated pipedrive

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -5,3 +5,7 @@ exports.all = (req, res) => {
 exports.findById = (req, res) => {
 
 };
+
+exports.callbackUpdated = (req, res) => {
+    
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const orderController = require('../controllers/orderController');
 
 router.get('/order', orderController.all);
 router.get('/order/:id', orderController.findById);
+router.post('/order/updated', orderController.callbackUpdated);
 
 module.exports = router;
