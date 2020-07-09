bc22d886a14c88384f7d240829ab12ea6cdcff5a
Andre Galdino <andre.galdinolima@gmail.com>
2020-07-09 10:28:54 -0300

    chore: auth database docker

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   mongo: {
     host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || 27017,
     database: process.env.DATABASE_DATABASE || 'database',
     user: process.env.DATABASE_USER || 'root',
     password: process.env.DATABASE_PASSWORD || 'secret',
