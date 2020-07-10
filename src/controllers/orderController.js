6945ef355c9d841620cb39f929fe9bb0f15d4d98
Andre Galdino <andre.galdinolima@gmail.com>
2020-07-10 02:00:29 -0300

    feat: webhook to create order on bling

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,3 +1,7 @@
+const { getOrderByExternalId } = require('../services/order')
+const createOrderOnBling = require('../services/createOrder')
+const {getDealById} = require('../services/pipedriveDeals');
+
 exports.all = (req, res) => {
 
 };
@@ -6,6 +10,22 @@ exports.findById = (req, res) => {
 
 };
 
-exports.callbackUpdated = (req, res) => {
-    
-}
+exports.callbackUpdated = async (req, res) => {
+  const { current: {
+    status,
+    id
+  }, previous } = req.body;
+  if (status !== 'won') {
+    return res.send()
+  }
+
+  const order = await getOrderByExternalId(id);
+  if (order) {
+    return res.send()
+  }
+
+  const {data: deal} = await getDealById(id)
+  const created = await createOrderOnBling(deal);
+
+  return res.send()
+};
